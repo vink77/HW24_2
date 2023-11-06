04c43ebef4b0d796eda78b0da510183dbf0ccc48
vink77 <vink.av@mail.ru>
2023-11-06 22:29:09 +0700

    finish2

--- users/urls.py
+++ users/urls.py
@@ -13,10 +13,10 @@ app_name = 'user'
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterUser.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-    #path('login/', LoginView.as_view(), name='login'),
-    path('password_recovery/', PasswordRecoveryView.as_view(), name='password_recovery'),
+ #   path('logout/', LogoutView.as_view(), name='logout'),
+ #   path('register/', RegisterUser.as_view(), name='register'),
+ #   path('profile/', ProfileView.as_view(), name='profile'),
+ #   #path('login/', LoginView.as_view(), name='login'),
+ #   path('password_recovery/', PasswordRecoveryView.as_view(), name='password_recovery'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
